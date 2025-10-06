57dc6a45acd24fa339fa921ae3803bae5213a6ac
Tochukwu Mezue <teejaymezue8@gmail.com>
2025-10-06 17:01:38 -0400

    Update About.jsx

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -41,6 +41,9 @@ export const About = () => {
                 <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                   Node.js
                 </span>
+                <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                  Express.js
+                </span>
               </div>
             </div>
           </div>
